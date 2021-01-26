3f854990b513b8410979fe7d0b23246be90b206e
si-osis <78008358+si-osis@users.noreply.github.com>
2021-01-26 12:04:23 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> ALS SI-7C |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Peta Situs</a>
+            <a href="#">Support</a>
+        </div>
+    </div>
+</div>
